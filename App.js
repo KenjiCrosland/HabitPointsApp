ec727e78dfdf743ea08ab3703f9c06311541377f
Kenji Crosland <kenji.crosland@gmail.com>
2018-10-22 18:13:05 -0700

    Change deprecated react-navigation methods

--- App.js
+++ App.js
@@ -3,41 +3,35 @@ import { Text, View, Button } from 'react-native';
 import HabitScreen from './components/HabitScreen';
 import HabitFormScreen from './components/HabitFormScreen';
 import StatsScreen from './components/StatsScreen';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-
-
-
-const DetailsScreen = ({navigation}) => (
+const DetailsScreen = ({ navigation }) => (
   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Text>Details Screen</Text>
   </View>
 );
 
-const HabitStackNavigator = StackNavigator({
-    HabitScreen: {
-      screen: HabitScreen,
-      navigationOptions: ({navigation}) => {
-        return {
-                title: 'Home',
-      headerRight: <Button title="New Habit" onPress={()=> navigation.navigate('HabitFormScreen')}/>
-        }
-
-     }
-    },
-    HabitFormScreen: {screen: HabitFormScreen}
-})
-
-const RootNavigator = TabNavigator({
-  HabitScreen: { 
-    screen: HabitStackNavigator
-    }, 
-  StatsScreen: {
-    screen: StatsScreen
+const HabitStackNavigator = createStackNavigator({
+  HabitScreen: {
+    screen: HabitScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Home',
+      headerRight: (
+        <Button title="New Habit" onPress={() => navigation.navigate('HabitFormScreen')} />
+      ),
+    }),
   },
+  HabitFormScreen: { screen: HabitFormScreen },
 });
 
-
+const RootNavigator = createBottomTabNavigator({
+  HabitScreen: {
+    screen: HabitStackNavigator,
+  },
+  StatsScreen: {
+    screen: StatsScreen,
+  },
+});
 
 export default RootNavigator;
